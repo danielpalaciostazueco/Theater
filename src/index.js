7b641f2392b169f37aa199dd9133913a40150eb5
danielpalaciostazueco <palaciostazuecodaniel@gmail.com>
2023-12-19 20:05:35 +0100

    Conexión a la base de datos

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { PORT, TYPE } from './utils/config.js'
 import rutasGenerales from './routes/general.routes.js'
 import morgan from 'morgan'
+import './utils/bbdd.js' // Cargar archivo para iniciar conexión  y si los bucles
 
 const app = express()
 app.use(morgan(TYPE))// Llamo a morgan
