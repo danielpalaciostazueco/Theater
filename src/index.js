d99205fbe3ea1a84c11292de1a52a790eab6a617
danielpalaciostazueco <palaciostazuecodaniel@gmail.com>
2023-12-21 18:09:39 +0100

    Anadir ruta obras

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { PORT, TYPE } from './utils/config.js'
 import rutasGenerales from './routes/general.routes.js'
+import obrasRuta from './routes/obras.route.js'
 import morgan from 'morgan'
 import './utils/bbdd.js' // Cargar archivo para iniciar conexión  y si los bucles
 
@@ -8,6 +9,8 @@ const app = express()
 app.use(morgan(TYPE))// Llamo a morgan
 app.use('/', rutasGenerales)// Uso la ruta general
 
+app.use('/obras', obrasRuta) // Ruta de obras
+app.use('/', rutasGenerales)// Uso la ruta general y poner al final
 app.listen(PORT, () => {
   console.info('listening on port ' + PORT)
 })
